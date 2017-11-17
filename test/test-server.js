639e10b7b3137c582e0b916bd2af78b9c3800668
lightsage88 <adrian.e.rosales@gmail.com>
2017-11-16 21:47:30 -0800

    solved it but made my own private git repository clone

--- test/test-server.js
+++ test/test-server.js
@@ -1,21 +1,15 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-
 const {app, runServer, closeServer} = require('../server');
-
 // this lets us use *should* style syntax in our tests
 // so we can do things like `(1 + 1).should.equal(2);`
 // http://chaijs.com/api/bdd/
 const should = chai.should();
-
 // This let's us make HTTP requests
 // in our tests.
 // see: https://github.com/chaijs/chai-http
 chai.use(chaiHttp);
-
-
 describe('Shopping List', function() {
-
   // Before our tests run, we activate the server. Our `runServer`
   // function returns a promise, and we return the that promise by
   // doing `return runServer`. If we didn't return a promise here,
@@ -24,7 +18,6 @@ describe('Shopping List', function() {
   before(function() {
     return runServer();
   });
-
   // although we only have one test module at the moment, we'll
   // close our server at the end of these tests. Otherwise,
   // if we add another test module that also has a `before` block
@@ -33,7 +26,6 @@ describe('Shopping List', function() {
   after(function() {
     return closeServer();
   });
-
   // test strategy:
   //   1. make request to `/shopping-list`
   //   2. inspect response object and prove has right code and have
@@ -49,7 +41,6 @@ describe('Shopping List', function() {
         res.should.have.status(200);
         res.should.be.json;
         res.body.should.be.a('array');
-
         // because we create three items on app load
         res.body.length.should.be.at.least(1);
         // each item should be an object with key/value pairs
@@ -61,7 +52,6 @@ describe('Shopping List', function() {
         });
       });
   });
-
   // test strategy:
   //  1. make a POST request with data for a new item
   //  2. inspect response object and prove it has right
@@ -82,7 +72,6 @@ describe('Shopping List', function() {
         res.body.should.deep.equal(Object.assign(newItem, {id: res.body.id}));
       });
   });
-
   // test strategy:
   //  1. initialize some update data (we won't have an `id` yet)
   //  2. make a GET request so we can get an item to update
@@ -99,7 +88,6 @@ describe('Shopping List', function() {
       name: 'foo',
       checked: true
     };
-
     return chai.request(app)
       // first have to get so we have an idea of object to update
       .get('/shopping-list')
@@ -119,7 +107,6 @@ describe('Shopping List', function() {
         res.should.have.status(204);
       });
   });
-
   // test strategy:
   //  1. GET a shopping list items so we can get ID of one
   //  to delete.
@@ -136,5 +123,69 @@ describe('Shopping List', function() {
       .then(function(res) {
         res.should.have.status(204);
       });
+  });
+});
+describe('Recipes', function(){
+  before(function(){
+    return runServer();
+  });
+  after(function(){
+    return closeServer();
+  });
+  it('should list recipes on GET', function(){
+    return chai.request(app)
+    .get('/recipes')
+    .then(function(res){
+      res.should.have.status(200);
+      res.body.should.be.a('array');
+      res.body.length.should.be.at.least(1);
+      const expectedKeys = ['id', 'name', 'ingredients'];
+      res.body.forEach(function(item){
+        item.should.be.a('object');
+        item.should.include.keys(expectedKeys);
+      });
+    });
+  });
+  it('should add an item on POST', function(){
+    const newRecipe = {name: 'crack cocaine', ingredients:['cocaine', 'baking soda', 'paprika', 'water']};
+    return chai.request(app)
+      .post('/recipes')
+      .send(newRecipe)
+      .then(function(res){
+        res.should.have.status(201);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        res.body.should.include.keys('id', 'name','ingredients');
+        res.body.id.should.not.be.null;
+        res.body.should.deep.equal(Object.assign(newRecipe, {id:res.body.id}));
+      });
+    });
+  it('should update recipes on PUT', function(){
+    const updateData = {
+      name: 'chocolate chip cookies',
+      ingredients: ['chocolate chips', 'eggs', 'horse blood']
+    };
+    return chai.request(app)
+      .get('/recipes')
+      .then(function(res){
+        updateData.id = res.body[0].id;
+        return chai.request(app)
+          .put(`/recipes/${updateData.id}`)
+          .send(updateData);
+      })
+      .then(function(res){
+        res.should.have.status(204);
+      });
+  });
+  it('should delete a recipe on DELETE', function(){
+    return chai.request(app)
+    .get('/recipes')
+    .then(function(res){
+      return chai.request(app)
+        .delete(`/recipes/${res.body[0].id}`);
+    })
+    .then(function(res){
+      res.should.have.status(204);
+    });
   });
 });
